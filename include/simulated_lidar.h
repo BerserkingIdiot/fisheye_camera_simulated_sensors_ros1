bb976eb7d0a1ac373c276bd6eeda2f44ef8a008d
Vítor Ventuzelos <skullventuzelos@hotmail.com>
2022-04-25 16:31:09 +0100

    First functional sensor

---
+++ include/simulated_lidar.h
@@ -0,0 +1,20 @@
+#ifndef SRC_SIMULATED_LIDAR_H
+#define SRC_SIMULATED_LIDAR_H
+
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <iostream>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <numeric>
+#include <math.h>
+
+#endif
