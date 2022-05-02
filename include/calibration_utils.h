9abdd1488b84544e46670b08a2bad2af3c25e211
Vítor Ventuzelos <skullventuzelos@hotmail.com>
2022-05-02 17:32:16 +0100

    Started camera calibration

---
+++ include/calibration_utils.h
@@ -0,0 +1,17 @@
+#ifndef SRC_SIMULATED_LIDAR_H
+#define SRC_SIMULATED_LIDAR_H
+
+#include <ros/ros.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <numeric>
+#include <math.h>
+
+#endif
